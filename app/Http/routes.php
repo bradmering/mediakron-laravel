0d14055aab2f40975db1e98d19116fe9b1baf9d1
Brad Mering <brad.mering@gmail.com>
2015-11-27 12:00:51 -0700

    Updating Routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,62 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(     '/',                         'HomeController@index');
+
+// Protected Routes by auth and acl middleware
+Route::group( [
+    'namespace' => 'Authenticated',
+    'middleware' => ['auth']],
+
+    function() use ($router){
+        # Dashboard Routes
+        Route::get(     '/profile',                 'ProfileController@profile');
+        Route::get(     '/profile/sites',           'ProfileController@sites');
+        Route::get(     '/profile/edit',            'ProfileController@edit');
+        Route::post(    '/profile/save',            'ProfileController@update');
+    }
+);
+
+
+
+// Protected Routes by auth and acl middleware
+Route::group( [
+    'prefix'   => 'admin',
+    'namespace'  => 'Admin',
+    'middleware'  => ['auth', 'acl']],
+
+    function() use ($router){
+        # Dashboard Routes
+        Route::resource(     '/site',                    'SiteController');
+        Route::resource(     '/user',                    'UserController');
+        Route::resource(     '/access',                  'AccessController');
+        Route::resource(     '/institution',             'InstitutionController');
+    }
+);
+
+
+
+Route::controllers([
+    'auth'      => 'Auth\AuthController',
+    'password'  => 'Auth\PasswordController',
+]);
+
+
+// Protected Routes by auth and acl middleware
+Route::group( [
+    'namespace' => 'Site',
+    'middleware' => ['mkaccess']],
+    'prefix' => '{site}',
+
+    function() use ($router){
+        # Dashboard Routes
+        
+        Route::post(         '/upload',                 'ImageController@style');
+        Route::resource(     '/user',                   'UserController');
+        Route::resource(     '/item',                   'ItemController');
+        Route::get(          '/cache',                  'CacheController@refresh');
+        Route::resource(     '/search',                 'Site\SearchController');
+    }
+);
+
+Route::get(                 '/files/{site}/{style}/{image}',      'ImageController@style');
